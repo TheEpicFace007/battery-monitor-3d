c49b08e670a766b39fe46b3de86337b83fd1b13b
Charlie <37189791+TheEpicFace007@users.noreply.github.com>
2023-02-05 20:33:21 -0500

    Added two utility func

--- include/utility.h
+++ include/utility.h
@@ -1,3 +1,5 @@
 #pragma once
 
 void sleep(int ms);
+void randomseed(int seed);
+int randint(int min, int max);
--- source/utility.cpp
+++ source/utility.cpp
@@ -1,7 +1,18 @@
 #include "utility.h"
+#include <random>
 #include <3ds.h>
 
 void sleep(int ms)
 {
     svcSleepThread(ms * 1000000);
+}
+
+void randomseed(int seed)
+{
+    srand(seed);
+}
+
+int randint(int min, int max)
+{
+    return rand() % (max - min + 1) + min;
 }
