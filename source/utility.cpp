45863c268eb1f55cd3105db2255f593dc2a31102
Charlie <37189791+TheEpicFace007@users.noreply.github.com>
2023-02-06 06:44:01 -0500

    Changed syntax

--- source/utility.cpp
+++ source/utility.cpp
@@ -2,17 +2,14 @@
 #include <random>
 #include <3ds.h>
 
-void sleep(int ms)
-{
+void sleep(int ms) {
     svcSleepThread(ms * 1000000);
 }
 
-void randomseed(int seed)
-{
+void randomseed(int seed) {
     srand(seed);
 }
 
-int randint(int min, int max)
-{
+int randint(int min, int max) {
     return rand() % (max - min + 1) + min;
 }
