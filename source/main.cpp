e4daad8c0d70f1ac8d5c0e0c130c72ffa865eb58
Charlie <37189791+TheEpicFace007@users.noreply.github.com>
2023-02-05 21:11:21 -0500

    Displayed battery info

--- source/main.cpp
+++ source/main.cpp
@@ -2,17 +2,16 @@
 #include <string.h>
 #include <stdio.h>
 #include <3ds.h>
+#include <iostream>
+#include "utility.h"
+#include "battery.h"
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
 	gfxInitDefault();
 	consoleInit(GFX_TOP, NULL);
 
-	printf("Hello, 3DS world!");
-
 	// Main loop
-	while (aptMainLoop())
-	{
+	while (aptMainLoop()) {
 		gspWaitForVBlank();
 		gfxSwapBuffers();
 		hidScanInput();
@@ -21,6 +20,15 @@ int main(int argc, char* argv[])
 		u32 kDown = hidKeysDown();
 		if (kDown & KEY_START)
 			break; // break in order to return to hbmenu
+		
+		// clear screen
+		consoleClear();
+		std::cout << "Battery: " << (int)getBatteryPercentage() << "%" << std::endl;
+		std::cout << "Plugged in: " << (isPluggedIn() ? "Yes" : "No") << std::endl;
+		if (isPluggedIn()) {
+			std::cout << "Finished charging: " << (didFinishedCharging() ? "Yes" : "No") << std::endl;
+		}
+		sleep(1000);
 	}
 
 	gfxExit();
