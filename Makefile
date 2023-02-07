18c73932e6cc50a59f58b1b8c83a73350f601914
Charlie <37189791+TheEpicFace007@users.noreply.github.com>
2023-02-07 13:28:04 -0500

    Drew text

--- Makefile
+++ Makefile
@@ -59,7 +59,7 @@ CXXFLAGS	:= $(CFLAGS) -fno-rtti -fno-exceptions -std=gnu++11
 ASFLAGS	:=	-g $(ARCH)
 LDFLAGS	=	-specs=3dsx.specs -g $(ARCH) -Wl,-Map,$(notdir $*.map)
 
-LIBS	:=  `$(PREFIX)pkg-config --libs sdl SDL_mixer` -lcitro3d -lctru -lm 
+LIBS	:=  -lcitro2d -lcitro3d -lctru -lm 
 
 #---------------------------------------------------------------------------------
 # list of directories containing libraries, this must be the top level containing
--- source/main.cpp
+++ source/main.cpp
@@ -2,34 +2,73 @@
 #include <iostream>
 #include "battery.h"
 #include "utility.h"
+#include <citro2d.h>
+#include <citro3d.h>
+
+bool statusPrintingLoop = true;
+void statusPrinintLoopTh(void*) {
+	std::cout << "Battery: " << (int)getBatteryPercentage() << "%" << std::endl;
+	std::cout << "Charging state: " << chargingStateToString(checkChagringState()) << std::endl;
+	std::cout << "Charger plugged in: " << (isChargerPluggedIn() ? "Yes" : "No") << std::endl;
+}
+
+C2D_TextBuf staticBuf, dynamicBuf;
+C2D_Text batteryPercentageText;
+
+void sceneInit() {
+	staticBuf = C2D_TextBufNew(4096);
+	dynamicBuf = C2D_TextBufNew(4096);
+}
+
+void sceneRender() {
+	C2D_TextBufClear(dynamicBuf);
+
+	// Display battery percentage
+	char batPercentage[10];
+	snprintf(batPercentage, 10, "%d%%", getBatteryPercentage());
+	
+	C2D_TextParse(&batteryPercentageText, dynamicBuf, batPercentage);
+	C2D_TextOptimize(&batteryPercentageText);
+	C2D_DrawText(&batteryPercentageText, C2D_AlignCenter, 0, 0, 0, 0.5f, 0.5f, 0.5f);
+}
+
+void sceneExit() {
+	C2D_TextBufDelete(staticBuf);
+	C2D_TextBufDelete(dynamicBuf);
+}
+
 
 int main(int argc, char **argv) {
 	gfxInitDefault();
 	mcuHwcInit();
 	ptmuInit();
-	consoleInit(GFX_TOP, NULL);
+	C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
+	C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
+	C2D_Prepare();
+	// Create screens
+	C3D_RenderTarget* top = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
 
+	// Initialize the scene
+	sceneInit();
 
 	while (aptMainLoop()){
 		hidScanInput();
 
 		u32 kDown = hidKeysDown();
+		u32 kHeld = hidKeysHeld();
 		if (kDown & KEY_START)
 			break;
-		// Clear the screen
-		for (int i = 0; i < 30; i++)
-			std::cout << std::endl;
-		std::cout << "Battery: " << getBatteryPercentage() << "%" << std::endl;
-		std::cout << "Charging state: " << chargingStateToString(checkChagringState()) << std::endl;
-		std::cout << "Charger plugged in: " << (isChargerPluggedIn() ? "Yes" : "No") << std::endl;
-		std::cout << "Press START to exit." << std::endl;
 
 
-		gfxFlushBuffers();
-		gfxSwapBuffers();
-		gspWaitForVBlank();
+		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+		C2D_TargetClear(top, C2D_Color32(0x00, 0x00, 0x00, 0xFF));
+		C2D_SceneBegin(top);
+		sceneRender();
+		C3D_FrameEnd(0);
 	}
-	
+	sceneExit();
+	statusPrintingLoop = false;
+	C3D_Fini();
 	ptmuExit();
 	mcuHwcExit();
 	gfxExit();
