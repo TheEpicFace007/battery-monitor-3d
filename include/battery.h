cf401b4455f6239c9cf74499cc0d045ffbf0c7d0
Charlie <37189791+TheEpicFace007@users.noreply.github.com>
2023-02-08 15:46:01 -0500

    Added comment to describe what the code do

--- include/battery.h
+++ include/battery.h
@@ -8,8 +8,20 @@ enum class ChargingState {
     FullyCharged
 };
 
-
+/**
+* Converts a ChargingState enum to a string
+*/
 std::string chargingStateToString(ChargingState state);
+/*
+* Gets the battery percentage
+*/
 u8 getBatteryPercentage();
+/*
+* Checks if the charger is plugged in
+*/
 bool isChargerPluggedIn();
+
+/*
+* Fetch the charging state
+*/
 ChargingState checkChagringState();
