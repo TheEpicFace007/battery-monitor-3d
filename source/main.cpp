65734190758aeb07648f3c61d62367c666c32239
Charlie <37189791+TheEpicFace007@users.noreply.github.com>
2023-02-07 19:48:28 -0500

    Added text

--- source/main.cpp
+++ source/main.cpp
@@ -4,6 +4,14 @@
 #include "utility.h"
 #include <citro2d.h>
 #include <citro3d.h>
+#include <thread>
+
+const u32 C_NeonGreen = C2D_Color32(0x00, 0xFF, 0x00, 0xFF);
+const u32 C_Green = C2D_Color32(0x00, 0x80, 0x00, 0xFF);
+const u32 C_LightGreen = C2D_Color32(0x90, 0xEE, 0x90, 0xFF);
+const u32 C_Yellow = C2D_Color32(0xFF, 0xFF, 0x00, 0xFF);
+const u32 C_Orange = C2D_Color32(0xFF, 0xA5, 0x00, 0xFF);
+const u32 C_Red = C2D_Color32(0xFF, 0x00, 0x00, 0xFF);
 
 bool statusPrintingLoop = true;
 void statusPrinintLoopTh(void*) {
@@ -13,11 +21,14 @@ void statusPrinintLoopTh(void*) {
 }
 
 C2D_TextBuf staticBuf, dynamicBuf;
-C2D_Text batteryPercentageText;
+C2D_Text batteryPercentageText, batteryLevelText;
 
 void sceneInit() {
 	staticBuf = C2D_TextBufNew(4096);
 	dynamicBuf = C2D_TextBufNew(4096);
+
+	C2D_TextParse(&batteryLevelText, staticBuf, "Battery level:");\
+	C2D_TextOptimize(&batteryLevelText);
 }
 
 void sceneRender() {
@@ -29,9 +40,10 @@ void sceneRender() {
 	
 	C2D_TextParse(&batteryPercentageText, dynamicBuf, batPercentage);
 	C2D_TextOptimize(&batteryPercentageText);
-	C2D_DrawText(&batteryPercentageText, C2D_AlignCenter, 200.0, 200, 0, 1, 1, 0.5f);
+	C2D_DrawText(&batteryPercentageText, C2D_AlignCenter, 200.0, (240 / 2)-5, 0, 0.8, 0.8, 0.5f);
 	ChargingState state = checkChagringState();
-	
+	// Display "Battery level:" over the battery percentage
+	C2D_DrawText(&batteryLevelText, C2D_AlignCenter, 200.0, (240 / 2) - 30, 0, 1, 1, 0.5f);
 }
 
 void sceneExit() {
@@ -61,11 +73,26 @@ int main(int argc, char **argv) {
 		if (kDown & KEY_START)
 			break;
 
-
+		// Render the scene
 		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
-		C2D_TargetClear(top, C2D_Color32(0xEE, 0xEE, 0xEE, 0xFF));
-		C2D_SceneBegin(top);
+		u32 colorToRender = C_NeonGreen;
+		if (getBatteryPercentage() < 100 && getBatteryPercentage() >= 80) {
+			colorToRender = C_Green;
+		} else if (getBatteryPercentage() < 80 && getBatteryPercentage() >= 50) {
+			colorToRender = C_LightGreen;
+		} else if (getBatteryPercentage() < 50 && getBatteryPercentage() >= 25) {
+			colorToRender = C_Yellow;
+		} else if (getBatteryPercentage() < 25 && getBatteryPercentage() >= 10) {
+			colorToRender = C_Orange;
+		} else if (getBatteryPercentage() < 10) {
+			colorToRender = C_Red;
+		}
+		C2D_SceneBegin(top); 
+		C2D_TargetClear(top, colorToRender);
 		sceneRender();
+		C2D_SceneBegin(bottom);
+		C2D_TargetClear(bottom, colorToRender);
+		C2D_SceneBegin(bottom);
 		C3D_FrameEnd(0);
 	}
 	sceneExit();
