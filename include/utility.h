2bda9d81e87b45bf0ab93d7084d924d93bd42562
Charlie <37189791+TheEpicFace007@users.noreply.github.com>
2023-02-16 16:47:18 -0500

    Added color interpolation func

--- include/utility.h
+++ include/utility.h
@@ -1,6 +1,8 @@
 #pragma once
+#include <vector>
+#include <3ds.h>
 
 void sleep(int ms);
 void randomseed(int seed);
 int randint(int min, int max);
-bool playWave(const char *path);
+std::vector<u8> interpolateColor(u32 color1, u32 color2, int steps);
